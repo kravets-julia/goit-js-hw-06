7ab6cc1cd1ece8bae2622e5ab50a04ca75dfdf64
kravets-julia <kravets_julia@i.ua>
2023-01-21 13:16:06 +0200

    Task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.getElementById ('ingredients');
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+    liEl.classList.add ('item');
+    ingredientsEl.append(liEl)
+}
+
+
+
