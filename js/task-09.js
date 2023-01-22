a1f65c9170f59fc5cde62d5d45a1a0553aad442a
kravets-julia <kravets_julia@i.ua>
2023-01-22 23:22:37 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs= {
+  spanEl:document.querySelector('.color'),
+  btnEl:document.querySelector('.change-color'),
+  bodyEl:document.querySelector('body')
+}
+
+refs.btnEl.addEventListener ('click', onchangeColor)
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onchangeColor () {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanEl.textContent = refs.bodyEl.style.backgroundColor
 }
