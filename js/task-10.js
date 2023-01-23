48b8b7266beacc283b1455a65c3279c42b182159
kravets-julia <kravets_julia@i.ua>
2023-01-23 23:57:21 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const divEl = document.getElementById('controls')
+const divBoxesEl = document.getElementById('boxes')
+const btnCreate = document.querySelector('[data-create]')
+const btnDestroy = document.querySelector('[data-destroy]')
+const inputEl = document.querySelector('input')
+
+btnCreate.addEventListener('click',  onBoxesCreate)
+btnDestroy.addEventListener('click', destroyBoxes)
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes (amount) {
+    for (let i = 0; i < amount; i+=1) {
+    const element = document.createElement('div');
+    element.style.width = 30 + (10*i)+'px';
+    element.style.height = 30 + (10*i)+'px';
+    element.style.backgroundColor =  getRandomHexColor();
+    divBoxesEl.appendChild(element)
+      }
+}
+
+function onBoxesCreate () {
+  const amount = inputEl.value;
+      createBoxes (amount);
+ }
+
+
+function destroyBoxes () {
+  divBoxesEl.innerHTML = '';
+}
+
