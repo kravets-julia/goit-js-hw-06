6fcd83a433f3c456750e3e733b5d26e5edb14f6b
kravets-julia <kravets_julia@i.ua>
2023-01-25 11:05:51 +0200

    corrected task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,13 +2,27 @@ const input = document.getElementById('validation-input');
 input.addEventListener ('blur', onInputBlur)
 
 function onInputBlur(event) {
-    if (event.target.value.length !== 6) {
-        input.classList.add('invalid')
+    if (event.currentTarget.value.trim().length === Number(input.dataset.length)) {
+        input.classList.add('valid')
+        input.classList.remove('invalid')
          }
     else
     {
-    input.classList.remove('invalid')
-    input.classList.add('valid')
-}
+    input.classList.add('invalid')
+    input.classList.remove('valid')
+    }
 return
 }
+console.log(input)
+
+// function onInputBlur(event) {
+//     if (event.target.value.length !== 6) {
+//         input.classList.add('invalid')
+//          }
+//     else
+//     {
+//     input.classList.remove('invalid')
+//     input.classList.add('valid')
+// }
+// return
+// }
