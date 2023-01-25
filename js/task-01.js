838f2b52758721a7a50ca8a9cc0b20552f106493
kravets-julia <kravets_julia@i.ua>
2023-01-25 10:20:08 +0200

    corrected task-01

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,13 @@ const itemEl = document.querySelectorAll('.item')
 console.log(`Number of categories: ${itemEl.length}`);
 
 
+// itemEl.forEach(element => {
+//     console.log(`Category: ${element.querySelector('h2').textContent}`)
+//     console.log(`Elements: ${element.querySelectorAll('h2 + ul >li').length}`)
+// });
+
 itemEl.forEach(element => {
-    console.log(`Category: ${element.querySelector('h2').textContent}`)
-    console.log(`Elements: ${element.querySelectorAll('h2 + ul >li').length}`)
+    console.log(`Category: ${element.firstElementChild.textContent}`)
+    console.log(`Elements: ${element.lastElementChild.children.length}`)
 });
     
