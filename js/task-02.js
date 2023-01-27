c9f84192bd2825251f0ceaf422f887db02ae90d3
kravets-julia <kravets_julia@i.ua>
2023-01-28 00:48:38 +0200

    corrected task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,34 @@ const ingredients = [
   'Condiments',
 ];
 
+console.log(ingredients)
 
 const ingredientsEl = document.getElementById ('ingredients');
 
-const listContent = ingredients. map((ingredients) => `<li class="item"> ${ingredients} </li>`). join ('');
+const array = [];
 
-ingredientsEl.insertAdjacentHTML("beforeend", listContent)
+for (let i = 0; i < ingredients.length; i+=1) {
+const liEl = document.createElement('li')
+    liEl.textContent = ingredients[i];
+    liEl.classList.add ('item');
 
-console.log(ingredientsEl.textContent)
+array.push(liEl)
+}
+
+ingredientsEl.append(...array)
+
+console.log(ingredientsEl)
+
+
+
+
+
+
+// const listContent = ingredients. map((ingredients) => `<li class="item"> ${ingredients} </li>`). join ('');
+
+// ingredientsEl.insertAdjacentHTML("beforeend", listContent)
+
+// console.log(ingredientsEl.textContent)
 
 
 // for (const ingredient of ingredients) {
